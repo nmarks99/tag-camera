ead92cbf4ea37a2cd99da291c6ad9b9c99d1a4d7
Nick Marks <nmarks@anl.gov>
2026-07-17 11:16:59 -0500

    fix tag locate

--- scripts/test_locate_tag.py
+++ scripts/test_locate_tag.py
@@ -2,7 +2,7 @@ from tag_camera import TagCamera
 import sys
 import time
 
-cam = TagCamera(device_index=1)
+cam = TagCamera(device_index=0)
 
 if len(sys.argv) != 2:
     print("Please provide ID to locate")
@@ -18,10 +18,8 @@ except:
 try:
     while True:
         tag = cam.locate_tag(id)
-        if (res):
-            x,y = cam.pixel_to_meters(res)
-            print(f"Tag {id}:")
-            print(f"Center: ({x:+8.2f},{y:+8.2f}) pixels")
+        if (tag):
+            print(f"Tag {id} distance = {tag['distance']}")
         time.sleep(0.1)
 except KeyboardInterrupt:
     pass
